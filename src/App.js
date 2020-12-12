1e670a2fde16acc1e0ec3233469e865bb0678f4e
bobbytito <michaelotito@gmail.com>
2020-12-12 22:16:59 +0100

    adding React routers to my project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import './App.css';
+import { Route } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
+
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
