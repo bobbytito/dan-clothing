301d255f954f4905d373de96d16ea9de4cbd8d75
bobbytito <michaelotito@gmail.com>
2020-12-13 20:57:04 +0100

    ...Sign in

--- src/pages/sign-in/sign-in.component.jsx
+++ src/pages/sign-in/sign-in.component.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 import './sign-in.styles.scss'
 
+import SignIn from '../../components/sign-in/sign-in.component'
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
-        SIGN IN
+        <SignIn />
     </div>
 )
 
