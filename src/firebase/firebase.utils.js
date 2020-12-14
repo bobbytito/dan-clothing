f5dc1d77d32c966ad8dbd2f6072d0b92c1e33ec4
bobbytito <michaelotito@gmail.com>
2020-12-14 21:06:04 +0100

    Firebase authentication setup

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth'
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAzIZ17OFcyTqAfsUuTA_5zgwIUDe5A7XM",
+    authDomain: "dan-clothing.firebaseapp.com",
+    projectId: "dan-clothing",
+    storageBucket: "dan-clothing.appspot.com",
+    messagingSenderId: "502191941609",
+    appId: "1:502191941609:web:389c6313906576a568cd78",
+    measurementId: "G-JXQJLY5TVX"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth =firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
