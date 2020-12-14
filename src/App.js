44004e9d72352608cd37ff439236235b375854f0
bobbytito <michaelotito@gmail.com>
2020-12-14 23:22:06 +0100

    ...Firebase authentication

--- src/App.js
+++ src/App.js
@@ -6,10 +6,34 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in/sign-in.component'
 import Header from './components/header/header.component.jsx'
+import { auth } from './firebase/firebase.utils'
 
 
+class App extends React.Component {
+  constructor(props) {
+    super(props) 
 
-function App() {
+      this.state= {
+        currentUser: null
+      }
+  }
+
+  unsubscribeFromAuth = null 
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+
   return (
     <div>
       <Header />
@@ -20,6 +44,7 @@ function App() {
       </Switch>
     </div>
   );
+  }
 }
 
 export default App;
