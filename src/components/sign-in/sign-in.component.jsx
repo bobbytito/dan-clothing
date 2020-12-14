81713a04329ba5d2d23f94d75dc3e66595283105
bobbytito <michaelotito@gmail.com>
2020-12-14 01:15:04 +0100

    form round up

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
 import './sign-in.styles.scss'
 
 class SignIn extends React.Component {
@@ -49,9 +50,8 @@ class SignIn extends React.Component {
                         label="password"
                         handleChange={this.handleChange} 
                     />
-                    <label>Password</label>
 
-                    <input type="submit" value="Submit Form" />
+                    <CustomButton type="submit"> Sign in </CustomButton>
                 </form>
             </div>
         )
