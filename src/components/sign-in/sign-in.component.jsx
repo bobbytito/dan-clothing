5f2083b2e108050b0b612b6db3def272b630c26a
bobbytito <michaelotito@gmail.com>
2020-12-15 17:10:14 +0100

    Google Authentication

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -52,12 +52,12 @@ class SignIn extends React.Component {
                         label="password"
                         handleChange={this.handleChange} 
                     />
-
-                    <CustomButton type="submit"> Sign in </CustomButton>
-                    <CustomButton onClick={signInWithGoogle}> 
-                        {' '}
-                        Sign in with Google {' '}
-                    </CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type="submit"> Sign in </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> 
+                            Sign in with Google
+                        </CustomButton>
+                    </div>  
                 </form>
             </div>
         )
