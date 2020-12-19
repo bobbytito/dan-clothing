a371690dd3a0959dc124a6692b9b64e8e647b89a
bobbytito <michaelotito@gmail.com>
2020-12-19 10:01:21 +0100

    cart-dropdown cart-item-reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -12,7 +12,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import './header.styles.scss'
 
-const Header =( {currentUser }) => (
+const Header =( {currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -36,12 +36,16 @@ const Header =( {currentUser }) => (
                 )}
                 <CartIcon />
         </div>
-        <CartDropdown />
+        {
+            hidden ? null :
+            <CartDropdown />
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
