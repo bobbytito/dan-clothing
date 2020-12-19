0c575d0ef6ad135c955242e8ee26e9d24645cb7e
bobbytito <michaelotito@gmail.com>
2020-12-19 09:33:54 +0100

    cart-dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,6 +8,7 @@ import { auth } from '../../firebase/firebase.utils'
 import { ReactComponent as Logo} from '../../assets/crown.svg'
 
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import './header.styles.scss'
 
@@ -35,6 +36,7 @@ const Header =( {currentUser }) => (
                 )}
                 <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 )
 
