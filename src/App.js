50a0993b8e42f1845401f198d3d9492e8e8bbd36
bobbytito <michaelotito@gmail.com>
2020-12-21 19:04:09 +0100

    checkout page with items total cost

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in/sign-in.component'
+import CheckoutPage from './pages/checkout/checkout.component'
+
 import Header from './components/header/header.component.jsx'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { selectCurrentUser} from './redux/user/user.selector'
@@ -49,6 +51,7 @@ class App extends React.Component {
       <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route path='/shop' component={ShopPage} />
+      <Route exact path='/checkout' component={CheckoutPage} />
       <Route exact path='/signin' 
       render={() => 
         this.props.currentUser ? (
