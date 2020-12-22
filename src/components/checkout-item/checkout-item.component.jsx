13d9e96b941e035bd7cc189223d0c882121817aa
bobbytito <michaelotito@gmail.com>
2020-12-22 15:13:10 +0100

    cart-volume indicator

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -8,18 +8,23 @@ import './checkout-item.styles.scss';
 
 const CheckoutItem = ({ cartItem, clearItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
-
+    return (
     <div className='checkout-item'>
         <div className='image-container'>
             <img src={imageUrl} alt='item'/>
         </div>
         <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
+        <span className='quantity'>
+            <div className='arrow'>&#10094;</div>
+            <span className='value'>{quantity}</span>
+            <div className='arrow'>&#10095;</div>
+        </span>
         <span className='price'>{price}</span>
         <div className='remove-button' onClick={() => clearItem(cartItem)}>
             &#10005;
         </div>
     </div>
+    )
 }
 
 const mapDispatchToProps = dispatch => ({
