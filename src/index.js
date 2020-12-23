2326488ab8f515a2f14a2a48e8c5857f9a67adcb
bobbytito <michaelotito@gmail.com>
2020-12-23 13:17:57 +0100

    redux persist

--- src/index.js
+++ src/index.js
@@ -7,14 +7,17 @@ import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import {store, persistor } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
     </React.StrictMode>,
   </Provider>,
