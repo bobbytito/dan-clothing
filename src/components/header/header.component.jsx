64786695c44876159a8e9febe972a9a0174b7f2b
bobbytito <michaelotito@gmail.com>
2021-01-07 13:17:56 +0100

    introducing styled components to my header and collection

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -14,37 +14,39 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import { selectCartHidden } from '../../redux/cart/cart.selectors'
 import { selectCurrentUser } from '../../redux/user//user.selector'
 
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink} from './header.styles';
+
 import './header.styles.scss'
 
 const Header =( {currentUser, hidden }) => (
-    <div className='header'>
-        <Link className='logo-container' to="/">
+    <HeaderContainer>
+        <LogoContainer to="/">
             <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to='/shop'>
                 SHOP
-            </Link>
-            <Link className='option' to='/shop'>
+            </OptionLink>
+            <OptionLink to='/shop'>
                 CONTACT
-            </Link>
+            </OptionLink>
             {
                 currentUser ? (
-                <div className='option' onClick={() => auth.signOut()}>
+                <OptionDiv onClick={() => auth.signOut()}>
                     SIGN OUT
-                </div>
+                </OptionDiv>
                 ) : (
-                <Link className='option' to='/signin'>
+                <OptionLink className='option' to='/signin'>
                     SIGN IN
-                </Link>
+                </OptionLink>
                 )}
                 <CartIcon />
-        </div>
+        </OptionsContainer>
         {
             hidden ? null :
             <CartDropdown />
         }
-    </div>
+    </HeaderContainer>
 )
 
 const mapStateToProps = createStructuredSelector({
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 import Directory from '../../components/directory/directory.component'
 
-import './homepage.styles.scss'
+import { HomePageContainer } from './homepage.styles';
 
-const HomePage = () => 
-     (
-        <div className= 'homepage'>
+const HomePage = () => (
+        <HomePageContainer>
             <Directory />
-        </div>
-    )
+        </HomePageContainer>
+    );
 
 export default HomePage
